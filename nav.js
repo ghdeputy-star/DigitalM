55fd2efe584bb734ccfc58ae022416f2c563fe15
ghdeputy-star <ghdeputy@iastate.edu>
2026-09-03 13:38:29 -0500

    Create nav.js

---
+++ nav.js
@@ -0,0 +1,15 @@
+// Navigation bar
+document.addEventListener("DOMContentLoaded", function () {
+    const nav = document.createElement("nav");
+
+    nav.innerHTML = `
+        <div class="nav-container">
+            <a href="index.html">About Me</a>
+            <a href="jobs.html">My Jobs</a>
+            <a href="personal.html">Personal</a>
+            <a href="school.html">School</a>
+        </div>
+    `;
+
+    document.body.prepend(nav);
+});
