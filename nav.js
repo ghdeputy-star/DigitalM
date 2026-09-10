2d15753d73f9f20efdf8135ee8942d87e49d5639
ghdeputy-star <ghdeputy@iastate.edu>
2026-09-10 13:43:20 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,11 +1,9 @@
 // Navigation bar
 document.addEventListener("DOMContentLoaded", function () {
-
     const nav = document.createElement("nav");
 
     nav.innerHTML = `
         <div class="nav-container">
-            <a href="index.html">About Me</a>
             <a href="jobs.html">My Jobs</a>
             <a href="personal.html">Personal</a>
             <a href="school.html">School</a>
